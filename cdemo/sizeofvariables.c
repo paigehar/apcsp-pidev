a28eee975206babbc90dcc5f862166fbfd095229
paigehar <paigeaharris@icloud.com>
2019-05-24 17:44:25 +0000

    test

---
+++ cdemo/sizeofvariables.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+
+int a = 5;
+
+char b = 'a';
+
+float c = 6.9420;
+
+double  d = 123.456;
+
+unsigned int e = 7;
+
+printf("Var a has a size of %d bytes\n", sizeof(a));
+
+printf("Var b has a size of %d bytes\n", sizeof(a));
+
+printf("Var c has a size of %d bytes\n", sizeof(c));
+
+printf("Var d has a size of %d bytes\n", sizeof(d));
+
+printf("Var e has a size of %d bytes\n", sizeof(e));
+}
+
