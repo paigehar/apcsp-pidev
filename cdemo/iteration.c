89878cafd3a3faafe899d38bd8d531d56d26415b
paigehar <paigeaharris@icloud.com>
2019-05-24 18:23:10 +0000

    test

---
+++ cdemo/iteration.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+	for (int i = 0; i < 100; i++)
+	{
+		printf("i is equal to %d\n", i);
+	}
+
+	if (i = 0)
+	{
+		print("the number does not divide evenly into 'div'\n");
+	}
+	else
+	{
+		print("the number does divide evenly into 'div'\n");
+	}
+
+}
