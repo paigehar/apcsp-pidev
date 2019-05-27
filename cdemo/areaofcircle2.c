eb206611c3fc48b001cede33f1f6a755c5fe6134
paigehar <50212329+paigehar@users.noreply.github.com>
2019-05-27 16:18:33 -0700

    Create areaofcircle2.c

---
+++ cdemo/areaofcircle2.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <math.h>
+
+float areaOfCircle(float r){
+  float area = r * r * M_PI;
+  return area;
+}
+
+float getBottomRadius(){
+  float rBot;
+  char input[256];
+  int check = 0;
+
+  while (check == 0) {
+    printf("Enter the lower bound for the radiuses: \n");
+    fgets(input, 256, stdin);
+    check = sscanf(input, "%f", &rBot);
+
+      if (check != 1) {
+        printf("Only enter numerical values please\n");
+      }  
+      
+  }
+return rBot;
+}
+
+float getTopRadius(){
+  float rTop;
+  char input[256];
+  int check = 0;
+
+  while (check == 0) {
+    printf("Enter the upper bound for the radiuses: \n");
+    fgets(input, 256, stdin);
+    check = sscanf(input, "%f", &rTop);
+
+      if (check != 1) {
+        printf("Only enter numerical values please\n");
+      }
+         
+      
+  }
+return rTop;
+}
+
+
+int main(){
+
+  float rBottom;
+  float rTop;
+
+  rBottom = getBottomRadius();
+  rTop = getTopRadius();
+
+
+  while (rBottom >= rTop) {
+    printf("Lowest radius must be lower than the highest radius\n");
+ printf("Input lowest radius: ");
+    scanf("%f", &rBottom);
+
+    printf("Input highest radius: ");
+    scanf("%f", &rTop);
+  }
+
+  for (float i = rBottom; i <= rTop; i++){
+    float a = areaOfCircle(i);
+    printf("%f\n",a);
+  }
+}
